c65250b9d825acf9b8dc97cd910f06832aa8cae2
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-01-30 00:36:19 -0500

    try1

--- 1.py
+++ 1.py
@@ -3,3 +3,21 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
+
+ret = 0
+
+mult3 = 3
+
+mult5 = 5
+
+while mult3 < 1000 or mult5 < 1000:
+	if mult5 == mult3:
+		ret = ret + mult3
+	else:
+		ret = ret + mult3 + mult5
+
+	mult3 = mult3 + 3
+
+	mult5 = mult5 + 5
+
+print(f"The sum of all the multiples of 3 or 5 below 1000 is {ret}")
