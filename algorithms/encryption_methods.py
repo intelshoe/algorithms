9f5d11e40ef9691475ec2e62250064f5269cfa42
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-01 18:34:45 -0500

    setup

--- algorithms/encryption_methods.py
+++ algorithms/encryption_methods.py
@@ -6,3 +6,20 @@
     Author: mp - info@stackevolve.com
 
 '''
+
+# Most effective way to encrypt data today?
+# 1) 2) 3) 4)
+
+
+
+
+
+# Secure VPN encryption
+
+
+
+
+
+
+
+# Secure firewalls and monitoring
