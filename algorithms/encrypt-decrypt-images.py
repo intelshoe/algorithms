b6bb3390af2611235755885f621526411f1662db
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-08 15:51:47 -0500

    cleanup

--- algorithms/encrypt-decrypt-images.py
+++ algorithms/encrypt-decrypt-images.py
@@ -4,6 +4,7 @@ Simple way to encrypt and decrypt an image
 '''
 import sys
 
+# take in path to image from command line argument
 image_location = sys.argv[1]
 
 print("Looking for test.png in home directory.")
@@ -12,7 +13,7 @@ fo = open(image_location, "rb")
 image = fo.read()
 fo.close()
 
-print("/n")
+print("")
 print("The secret key is 12.")
 image = bytearray(image)
 key = 12
@@ -20,7 +21,7 @@ key = 12
 for index, value in enumerate(image):
    image[index] = value^key
 
-print("/n")
+print("")
 print("Image encrypted. Saving file as etest.png")
 fo = open("etest.png", "wb")
 fo.write(image)
@@ -34,5 +35,5 @@ for index, value in enumerate(image):
 fo = open("dtest.png", "wb")
 fo.write(image)
 fo.close()
-print("/n")
+print("")
 print("The images is decrypted and saved as dtest.png")
