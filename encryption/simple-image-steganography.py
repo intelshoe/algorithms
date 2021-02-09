7f3d26964265a39748af104a01d23a99975ff82e
Just A Coder <61443129+mellowpuppy@users.noreply.github.com>
2021-02-08 19:26:00 -0500

    added encrypt msg

--- encryption/simple-image-steganography.py
+++ encryption/simple-image-steganography.py
@@ -1,17 +1,25 @@
 '''
 Simple way to hide message in image
 
-Requires cryptosteganography and python version 3.8:
+Requires cryptography module, cryptosteganography and python version 3.8:
 https://github.com/computationalcore/cryptosteganography
 
 '''
-
+from cryptography.fernet import Fernet 
 from cryptosteganography import CryptoSteganography
 
 # generate a key and store it in file named s.key
 key = Fernet.generate_key()
 with open("s.key", "wb") as keyfile:
 	keyfile.write(key)
+
+# secret message to store in image
+msg = "The secret word is gravy!".encode()
+
+# encrypt
+f = Fernet(key)
+emsg = f.encrypt(msg)
+print(emsg)
 
 # to open s.key once stored
 # return open("s.key", "rb").read()
